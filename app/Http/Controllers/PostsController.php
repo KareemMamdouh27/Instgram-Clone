b3c3f49a84ce1774427b8e7191fbc7869e835647
KareemMamdouh27 <kareemmamdouh54@gmail.com>
2021-06-16 20:25:40 +0200

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'title' => '',
+            'caption' => 'required',
+            'image'   => 'required|image',
+        ]);
+
+        $imagePath = request('image')->store('uploads', 'public');
+
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image'   => $imagePath,
+        ]);
+
+        return redirect('/profile/'. auth()->user()->id);   
+
+        dd(request()->all());   
+    }
+}
---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row" style="height: 230px">
+        <div class="col-3">
+            <img src="../photos/profile.jpg" class="rounded-circle p-5 h-25" alt="" srcset="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> Posts</div>
+                <div class="pr-5"><strong>23k</strong> Followers</div>
+                <div class="pr-5"><strong>212</strong> Following</div>
+            </div>
+            <div class="pt-3"><strong>{{ $user->profile->title }}</strong></div>
+            <p> {{ $user->profile->description }} </p>
+            <div><a href={{ $user->profile->url }}>Facebook.com/kareem.kimooz27</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        @foreach ($user->posts as $post)
+            <div class="col-4">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </div>
+        @endforeach
+    </div>
+
+</div>
+@endsection
